c141b353cfe1ba457b4d5c4326c5b3e9e3ee3a1a
StaniGit <152064366+StaniGit@users.noreply.github.com>
2025-04-28 10:27:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-let inputs = document.querySelectorAll(".input");
-
-function focusFunc() {
-  let parent = this.parentNode;
-  parent.classList.add("focus");
-}
-
-function blurFunc() {
-  let parent = this.parentNode;
-  if (this.value == "") {
-    parent.classList.remove("focus");
-  }
-}
-
-inputs.forEach((input) => {
-  input.addEventListener("focus",focusFunc);
-  input.addEventListener("blur",blurFunc);
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
 });
 
-function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
-
-document.querySelector('form').addEventListener('submit', function() {
-  document.querySelector('input[type="submit"]').disabled = true;
-})
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
